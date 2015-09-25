a220201bb1aafa5085f4fee95b79348fef7702cc
Florian Müller <florian@tomueller.de>
2015-09-25 09:55:10 +0200

    Minor refactoring

--- src/main/scala/com/github/sweb/machinelearning/MLMatrix.scala
+++ src/main/scala/com/github/sweb/machinelearning/MLMatrix.scala
@@ -8,33 +8,19 @@ import org.ejml.simple.SimpleMatrix
  */
 case class MLMatrix(data: SimpleMatrix) {
 
-  def this(rawData: Array[Array[Double]]) = {
-    this(new SimpleMatrix(rawData))
-  }
+  def this(rawData: Array[Array[Double]]) = this(new SimpleMatrix(rawData))
 
-  def apply(row: Int, col:Int): Double = {
-    data.get(row, col)
-  }
+  def apply(row: Int, col:Int): Double = data.get(row, col)
 
-  def transpose(): MLMatrix = {
-    MLMatrix(data.transpose())
-  }
+  def transpose(): MLMatrix = MLMatrix(data.transpose())
 
-  def invert(): MLMatrix = {
-    MLMatrix(data.invert())
-  }
+  def invert(): MLMatrix = MLMatrix(data.invert())
 
-  def *(otherMatrix: MLMatrix): MLMatrix = {
-    MLMatrix(data.mult(otherMatrix.data))
-  }
+  def *(that: MLMatrix): MLMatrix = MLMatrix(data.mult(that.data))
 
-  def *(otherMatrix: MLVector): MLVector = {
-    MLVector(data.mult(otherMatrix.data))
-  }
+  def *(that: MLVector): MLVector = MLVector(data.mult(that.data))
 
-  def -(otherMatrix: MLMatrix): MLMatrix = {
-    MLMatrix(data.minus(otherMatrix.data))
-  }
+  def -(that: MLMatrix): MLMatrix = MLMatrix(data.minus(that.data))
 
   def numberOfRows = data.getMatrix.getNumRows
   def numberOfCols = data.getMatrix.getNumCols
@@ -50,25 +36,15 @@ case class MLVector(data: SimpleMatrix) {
     this(new SimpleMatrix(Array(rawData)).transpose())
   }
   
-  def apply(index: Int): Double = {
-    data.get(index)
-  }
+  def apply(index: Int): Double = data.get(index)
 
-  def transpose(): MLVector = {
-    MLVector(data.transpose())
-  }
+  def transpose(): MLVector = MLVector(data.transpose())
 
-  def *(otherMatrix: MLVector): MLVector = {
-    MLVector(data.mult(otherMatrix.data))
-  }
+  def *(that: MLVector): MLVector = MLVector(data.mult(that.data))
 
-  def *(otherMatrix: MLMatrix): MLVector = {
-    MLVector(data.mult(otherMatrix.data))
-  }
+  def *(that: MLMatrix): MLVector = MLVector(data.mult(that.data))
 
-  def -(otherMatrix: MLVector): MLVector = {
-    MLVector(data.minus(otherMatrix.data))
-  }
+  def -(that: MLVector): MLVector = MLVector(data.minus(that.data))
 
   def numberOfItems = data.getMatrix.getNumElements
 
