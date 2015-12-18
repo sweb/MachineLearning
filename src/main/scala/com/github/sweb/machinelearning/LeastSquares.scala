83b82fa1547412e103568fd146e2b0f2dc323c4c
Florian Müller <f.mueller@emetriq.com>
2015-12-18 09:36:56 +0100

    Modifications with current state of progress

--- src/main/scala/com/github/sweb/machinelearning/LeastSquares.scala
+++ src/main/scala/com/github/sweb/machinelearning/LeastSquares.scala
@@ -1,11 +1,11 @@
 package com.github.sweb.machinelearning
 
 /**
- * Implementation of least squares, according to chapter 3.2 of ESL II.
- */
+  * Implementation of least squares, according to chapter 3.2 of ESL II.
+  */
 case class LeastSquares(featureMatrix: Array[Array[Double]], observations: Array[Double], addIntercept: Boolean) {
   val processedFeatures = preprocessFeatures(featureMatrix)
-  val X = MLMatrix(processedFeatures)
+  val X = LeastSquares.standardize(MLMatrix(processedFeatures))
   val y = MLVector(observations)
 
   lazy val fittedParameters = solve()
@@ -16,7 +16,7 @@ case class LeastSquares(featureMatrix: Array[Array[Double]], observations: Array
       "Number of feature rows does not equal number of observations")
 
     val T = X.transpose
-    val beta = ( T * X ).invert()* T * y
+    val beta = (T * X).invert() * T * y
     beta.getData()
   }
 
@@ -30,7 +30,7 @@ case class LeastSquares(featureMatrix: Array[Array[Double]], observations: Array
 
   def residualSumOfSquares(parameters: Array[Double]): Double = {
     val beta = MLVector(parameters)
-    val rss = (y - (X * beta) ).transpose * (y -(X * beta))
+    val rss = (y - (X * beta)).transpose * (y - (X * beta))
     rss(0)
   }
 
@@ -56,22 +56,24 @@ object LeastSquares {
     val columns = (0 until featureMatrix.numberOfCols).map(featureMatrix.data.extractVector(false, _).getMatrix.getData).toList
     val sizes = columns.map(_.size)
     val means = columns.map(col => col.sum / col.size)
-    val standardDeviations = columns.zip(means).map(tuple => Math.sqrt(tuple._1.map(v => Math.pow(v - tuple._2, 2)).sum / (tuple._1.size - 1)))
-    val reducedByMean = columns.zip(means).map(tuple => tuple._1.map(_ - tuple._2))
-    val standardized = reducedByMean.zip(standardDeviations).map(tuple => tuple._1.map(_ / tuple._2))
+    val standardDeviations = columns.zip(means)
+      .map(tuple => Math.sqrt(tuple._1.map(v => Math.pow(v - tuple._2, 2)).sum / (tuple._1.size - 1)))
 
     val allInOne = columns.zip(means).zip(standardDeviations)
       .map(x => (x._1._1, x._1._2, x._2))
-    .map(triple => triple match {
-      case (column, mean, std) => {
-        if (std == 0.0) {
-          column
-        } else {
-          column.map(x => (x - mean) / std)
+      .map(triple => triple match {
+        case (column, mean, std) => {
+          if (std == 0.0) {
+            column
+          } else {
+            val c = column.map(x => (x - mean) / std)
+            println(c.mkString(","))
+            c
+          }
         }
-      }
-    })
+      })
 
+    //println(MLMatrix(allInOne.toArray).transpose.data.toString)
     MLMatrix(allInOne.toArray).transpose
   }
 }
